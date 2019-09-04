468b6fdb8921b9731a94656b9898fdb7c5422ea3
cheneyweb <457299596@qq.com>
2019-09-05 00:04:44 +0800

    初始

---
+++ src/script/BtnTest.js
@@ -0,0 +1,18 @@
+/**
+ * 脚本
+ */
+export default class BtnTest extends Laya.Script {
+    constructor() { super(); }
+    onEnable() {
+        console.log('按钮初始化')
+    }
+
+    onTriggerEnter(other, self, contact) {
+    }
+
+    onUpdate() {
+    }
+
+    onDisable() {
+    }
+}
---
+++ src/view/GameView.js
@@ -0,0 +1,14 @@
+/**
+ * 游戏主界面类
+ */
+export default class GameView extends Laya.Scene {
+    constructor() {
+        super()
+    }
+    onEnable() {
+    }
+    onClosed() {
+    }
+    onMouseDown(e) {
+    }
+}
